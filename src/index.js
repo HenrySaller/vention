e1a856755ffca7fd522d7af098f6e273801ee879
Henry Saller <henrysaller.msg@gmail.com>
2018-03-15 16:57:44 +0200

    add preact boilerplate

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { h, render } from 'preact';
+
+import App from './App';
+
+import registerServiceWorker from './registerServiceWorker';
+
+render(<App />, document.getElementById('root'));
+registerServiceWorker();
